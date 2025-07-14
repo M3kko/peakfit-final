8568be987a55d9622263d41ea275c1515475bfe6
M3kko <Brody@imaximus.com>
2025-07-14 14:49:13 -0500

    fixed cursor

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -351,6 +351,9 @@ class _AuthScreenState extends State<AuthScreen> with TickerProviderStateMixin {
         controller: controller,
         obscureText: obscureText,
         keyboardType: keyboardType,
+        cursorColor: Colors.white,
+        cursorWidth: 2.5,
+        cursorRadius: const Radius.circular(1),
         style: const TextStyle(
           color: Colors.white,
           fontSize: 16,
